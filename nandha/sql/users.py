484599c68e95c853869f1ed164b67f7061d15831
TheDarkSaiyan <nandhasigma@gmail.com>
2024-07-03 21:41:34 +0530

    Add files via upload

--- nandha/sql/users.py
+++ nandha/sql/users.py
@@ -1,13 +1,21 @@
 from nandha.sql import SESSION, BASE
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String, Boolean, BigInteger
 import threading
 
 class Users(BASE):
     __tablename__ = 'users'
-    user_id = Column(Integer, primary_key=True)
-    
-    def __init__(self, user_id):
-         self.user_id = user_id
+    user_id = Column(BigInteger, primary_key=True)
+    first_name = Column(String)
+    username = Column(String)
+    is_bot = Column(Boolean)
+    language_code = Column(String)
+
+    def __init__(self, user_id, first_name, username, is_bot, language_code):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.username = username
+        self.is_bot = is_bot
+        self.language_code = language_code
 
 # Create the table if it doesn't exist
 Users.__table__.create(checkfirst=True)
@@ -15,14 +23,24 @@ Users.__table__.create(checkfirst=True)
 # Thread-safe lock for database operations
 INSERTION_LOCK = threading.RLock()
 
-def add_user(user_id):
+
+def add_user(obj):
     with INSERTION_LOCK:
         try:
+            user_id = obj['id']
             user = SESSION.query(Users).get(user_id)
             if not user:
-                user = Users(user_id=user_id)
+                user = Users(
+                    user_id=user_id,
+                    first_name=obj.get('first_name'),
+                    username=obj.get('username'),
+                    is_bot=obj.get('is_bot'),
+                    language_code=obj.get('language_code')
+                )
                 SESSION.add(user)
                 SESSION.commit()
+              
+            return    
         except Exception as e:
             SESSION.rollback()
             print(f"Error adding user: {e}")
@@ -34,13 +52,29 @@ def remove_user(user_id):
             if user:
                 SESSION.delete(user)
                 SESSION.commit()
+                
         except Exception as e:
             SESSION.rollback()
             print(f"Error removing user: {e}")
 
+
+def get_user_data(user_id):
+    try:
+        user = SESSION.query(Users).get(user_id)
+        data = {}
+        if user:
+            data = { key: value for key, value in user.__dict__.items() if not key.startswith('_') }
+        return data
+    except Exception as e:
+        print(f"Error getting user: {e}")
+        return data
+
+
+
+
 def get_all_users():
     try:
-        return [user[0] for user in SESSION.query(Users.user_id).all()]
+        return [user.user_id for user in SESSION.query(Users).all()]
     except Exception as e:
         print(f"Error getting all users: {e}")
         return []
