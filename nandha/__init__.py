6d054b92d0b7f1a89ec95a2789b0b188b7897b72
TheDarkSaiyan <nandhasigma@gmail.com>
2024-07-03 21:42:36 +0530

    Add files via upload

--- nandha/__init__.py
+++ nandha/__init__.py
@@ -1,22 +1,27 @@
 
 
 
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, PicklePersistence, ContextTypes, CommandHandler
 from config import *
 
 
 import logging
+import aiohttp
 
 
 LOGGER = logging.getLogger(__name__)
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+FORMAT = f"[Bot] %(message)s"
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
+              logging.StreamHandler()], format=FORMAT)
+
+
 
 logging.getLogger('httpx').setLevel(logging.WARNING)
 
 
+# Clients
+persistence = PicklePersistence(filepath='db')
+app = ApplicationBuilder().token(TOKEN).persistence(persistence).build()
 
-app = ApplicationBuilder().token(TOKEN).build()
+aiohttpsession = aiohttp.ClientSession()
--- nandha/__main__.py
+++ nandha/__main__.py
@@ -27,8 +27,7 @@ def import_plugins(package):
         importlib.import_module(full_name)
         imported_modules.append(full_name)
 
-    logging.info(f"Successfully imported {len(imported_modules)} modules: [{', '.join(imported_modules)}]")
-
+    logging.info(f"Successfully imported {len(imported_modules)} modules: [{', '.join(module.rsplit('.', 1)[-1] for module in imported_modules)}]")
 
 if __name__ == '__main__':
     from nandha import app
