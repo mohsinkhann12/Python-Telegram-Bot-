66b18b15abfa7e69f92c4b37e3fcf767392353ba
TheDarkSaiyan <nandhasigma@gmail.com>
2024-07-03 21:37:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,11 @@ DB_URL = os.getenv('DB_URL')
 SUPPORT_CHAT = '@NandhaChat'
 
 DEV_LIST = [
-  5696053228
+  7402274873, # t.me/darksaiyanxd
+  5696053228, # t.me/nandha
+  
+# Want to be dev ? contact nandhachat.t.me 
+
 ]
 
 
